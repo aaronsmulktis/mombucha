b8faff16712a6b77ef48c93af33556477a9b1e04
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-12-30 15:12:38 -0500

    update url

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php 
 
+update_option( 'siteurl', 'https://mombucha.com' );
+update_option( 'home', 'https://mombucha.com' );
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
-
-update_option( 'siteurl', 'https://mombucha.com' );
-update_option( 'home', 'https://mombucha.com' );
 
 ?>
